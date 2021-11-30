f24b369013ee4053f083ba2d71d12340ed74e403
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-11-30 10:55:33 -0500

    finished emp prompts, created employee objects and added to array

--- index.js
+++ index.js
@@ -42,4 +42,119 @@ const managerPrompts = () => {
     })
 };
 
-managerPrompts();
+const employeePrompts = () => {
+    console.log(`
+    =================
+    Adding employees to the team
+    =================
+    `);
+
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'title',
+            message: 'Employee title:',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Employee name:', 
+            validate: empName => {
+                if (empName) {
+                    return true;
+                } else {
+                    console.log ('Please enter employee name!');
+                    return false; 
+                }
+            }
+        }
+        ,
+        {
+            type: 'input',
+            name: 'empID',
+            message: 'Employee ID:',
+            validate: empID => {
+                if  (isNaN(empID)) {
+                    console.log ('Please enter employee ID!')
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Employee email:',
+            validate: email => {
+                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+                    return true;
+                } else {
+                    console.log ('Please enter a valid email!')
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Employee github username:',
+            when: (input) => input.title === 'Engineer',
+            validate: githubName => {
+                if (githubName ) {
+                    return true;
+                } else {
+                    console.log ('Please enter an employee github username!')
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Intern school:',
+            when: (input) => input.title === 'Intern',
+            validate: schoolName => {
+                if (schoolName) {
+                    return true;
+                } else {
+                    console.log ('Please enter the intern school!')
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmp',
+            message: 'Would you like to add more team members?',
+            default: false
+        }
+    ])
+    .then(employeeData => {
+        // data for employee types 
+
+        let { name, empID, email, title, github, school, confirmAddEmp } = employeeData; 
+
+        if (title === "Engineer") {
+            employee = new Engineer (name, empID, email, github);
+
+        } else if (title === "Intern") {
+            employee = new Intern (name, empID, email, school);
+        }
+
+        teamArr.push(employee); 
+        // console.log(teamArr);
+
+        if (confirmAddEmp) {
+            return employeePrompts(teamArr); 
+        } else {
+            return teamArr;
+        }
+        
+    })
+    
+};
+
+managerPrompts()
+.then(employeePrompts)
+
+
